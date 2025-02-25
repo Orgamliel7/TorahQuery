2d2a27b59942a365e731cbdc262718b66f43ce76
Orgamliel7 <orgamliel777@gmail.com>
2025-02-25 14:37:51 +0200

    modify some config files, now mvn build is working but spring not yet

--- backend/src/main/java/com/torahsearch/service/SearchService.java
+++ backend/src/main/java/com/torahsearch/service/SearchService.java
@@ -7,6 +7,7 @@ import com.torahsearch.repository.AnswerRepository;
 import com.torahsearch.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.torahsearch.search.LuceneService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
