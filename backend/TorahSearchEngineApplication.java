a526be94205cf280bcd0109ad0ae7575b5b67a98
Orgamliel7 <orgamliel777@gmail.com>
2025-02-25 12:17:59 +0200

    application.properties + TorahSearchEngineApplication.java files for backend

---
+++ backend/TorahSearchEngineApplication.java
@@ -0,0 +1,11 @@
+package com.torahsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorahSearchEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TorahSearchEngineApplication.class, args);
+    }
+}
